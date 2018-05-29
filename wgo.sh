9ac11d68a8b8063829be129b8c1b4016ba79e362
Wade Liu <wadeliu@qnap.com>
2018-05-29 15:41:39 +0100

    Alwarys install

--- wgo.sh
+++ wgo.sh
@@ -7,23 +7,20 @@ fi
 
 WGOSRC="${HOME}/.wgosrc"
 WGOLIST="${HOME}/.wgolist"
-if [ -f ${WGOSRC} ]; then
-    echo "Already installed"
-    return
-fi
-BASHRCFILE=~/.profile
+
+PROFILE_FILE=~/.profile
 
 if [ "`basename $SHELL`" = "zsh" ]; then
-    BASHRCFILE=~/.zshrc
+    PROFILE_FILE=~/.zshrc
 fi
 
 echo "if [ -z \"\$1\" ]; then cat ${WGOLIST}  | sed 's/alias //g' | sed 's/cd //g'; else alias \$1=\"cd \`pwd\`\"; echo "alias \$1=\\\"cd \`pwd\`\\\"" >> ${WGOLIST}; fi" > ${WGOSRC}
-grep "wgo" ${BASHRCFILE} | grep -v grep
+grep "wgo" ${PROFILE_FILE} | grep -v grep
 if [ "$?" != "0" ];then
     touch ${WGOLIST}
     alias wgo="source ${WGOSRC}" > /dev/null
-    echo "alias wgo=\"source ${WGOSRC}\"" >> ${BASHRCFILE}
-    echo "source ${WGOLIST}" >> ${BASHRCFILE}
+    echo "alias wgo=\"source ${WGOSRC}\"" >> ${PROFILE_FILE}
+    echo "source ${WGOLIST}" >> ${PROFILE_FILE}
 fi
 
 echo "Install Success"
