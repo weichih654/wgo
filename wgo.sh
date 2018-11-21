acb497baec46d59fd5a7399bc46d5bf516fa17e1
Wade Liu <wadeliu@qnap.com>
2018-11-21 02:47:03 +0000

    Add wgorm

--- wgo.sh
+++ wgo.sh
@@ -6,6 +6,7 @@ if [ $called == $0 ]; then
 fi 
 
 WGOSRC="${HOME}/.wgosrc"
+WGORM="${HOME}/.wgorm"
 WGOLIST="${HOME}/.wgolist"
 
 PROFILE_FILE=~/.profile
@@ -15,13 +16,17 @@ if [ "`basename $SHELL`" = "zsh" ]; then
 fi
 
 echo "if [ -z \"\$1\" ]; then cat ${WGOLIST}  | sed 's/alias //g' | sed 's/cd //g'; else alias \$1=\"cd \\\"\`pwd\`\\\"\"; echo "alias \$1=\\\"cd \\\\\\\"\"\`pwd\`\\\\\\\"\\\"\"" >> ${WGOLIST}; fi" > ${WGOSRC}
+
+echo 'tag=$1; unalias ${tag} 2>/dev/null; wgolist=~/.wgolist; sed -i "/^alias ${tag}=.*/d" ${wgolist};' > ${WGORM}
+alias wgo="source ${WGOSRC}" > /dev/null
+alias wgorm="source ${WGORM}" > /dev/null
+
 grep "wgo" ${PROFILE_FILE} | grep -v grep
 if [ "$?" != "0" ];then
-    touch ${WGOLIST}
-    alias wgo="source ${WGOSRC}" > /dev/null
     echo "alias wgo=\"source ${WGOSRC}\"" >> ${PROFILE_FILE}
     echo "source ${WGOLIST}" >> ${PROFILE_FILE}
 fi
+touch ${WGOLIST}
 source ${WGOLIST}
 
 echo "Install Success"
